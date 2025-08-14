650aa0217df752b5c3fac140cf2e2654bccfd822
NETTAX-TN <nataf2057@gmail.com>
2025-08-14 11:27:07 -0400

    mate

--- mate.py
+++ mate.py
@@ -1,9 +1,36 @@
 '''Escribe un código que pida al usuario ingresar un número real y determine si el número ingresado es positivo, negativo o cero.'''
-numero1=int(input("ingrese un numero "))
-if numero1 >= 0 :
-    print("numero positivo")
-elif numero1 <= 0 :
-    print("numero negativo")
-elif numero1 == 0 :
-    print("numero es cero")
+# numero1=float(input("ingrese un numero "))
+# if numero1 >= 0 :
+#     print("numero positivo")
+# elif numero1 <= 0 :
+#     print("numero negativo")
+# elif numero1 == 0 :
+#     print("numero es cero")
 
+# problema 2
+# numero=int(input("ingrese numero "))
+# if numer % 0==2:
+#     print("par")
+# else:
+#     print("impar")
+
+#problema 3
+# n=1
+# n1=0
+# while n<=100:
+#     n1+=n
+#     n+=1
+# print(n1)
+
+# problema 4
+# conta="hola"
+# contaseña=input("ingrese su contra")
+# while conta != contaseña:
+#     contaseña=input("ingrese de nuevo la contra")
+
+# problema 5
+# numero=int(input("ingrese un numero"))
+# for i in range(numero):
+#     for o in range(12):
+
+#problema 6
